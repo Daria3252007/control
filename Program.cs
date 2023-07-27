bca9cda14daf6da043181f5f4fc5142c82b98da8
Daria_lev <375293252007@yandex.by>
2023-07-27 11:32:04 +0300

    com1

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿int[,] RandArr()
+{
+    Console.WriteLine();
+    Console.WriteLine("Generating a 2d array..");
+    Console.Write("Enter array rows number: ");
+    int line = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter array columns number: ");
+    int col = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter minimal range value: ");
+    int min = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter maximum range value: ");
+    int max = int.Parse(Console.ReadLine()!);
+
+    int[,] arr = new int[line, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[i, j] = rand.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+void PrintArr(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void LineSwap(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        (arr[0,j], arr[arr.GetLength(0)-1,j])=(arr[arr.GetLength(0)-1,j],arr[0,j]);
+    }
+}
+int[,] array=RandArr();
+PrintArr(array);
+Console.WriteLine();
+LineSwap(array);
+PrintArr(array);
+
